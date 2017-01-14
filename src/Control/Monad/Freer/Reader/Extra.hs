f0477927dd0f78aaf9975982ba232bbe1e3474cf
Peter Trško <peter.trsko@gmail.com>
2017-01-14 13:05:37 +0100

    Lens support for Reader and State effect

--- src/Control/Monad/Freer/Reader/Extra.hs
+++ src/Control/Monad/Freer/Reader/Extra.hs
@@ -12,17 +12,80 @@
 -- <https://jaspervdj.be/posts/2015-01-20-haskell-design-patterns-extended-modules.html Extended module>
 -- built on top of "Control.Monad.Freer.Reader".
 module Control.Monad.Freer.Reader.Extra
-    ( module Control.Monad.Freer.Reader
+    (
+    -- * Re-export Module That's Beeing Extended
+    --
+    -- | Everything is beeing re-exported from it except 'asks', which is
+    -- reimplemented here with more general type signature.
+      module Control.Monad.Freer.Reader
+
+    -- * Effect Operations
     , asks
+
+    -- ** Support For Lenses
+    , viewEff
+    , viewsEff
+    , iviewEff
+    , iviewsEff
     )
   where
 
+import Data.Function (($))
 import Data.Functor ((<$>))
+import Data.Functor.Const (Const(Const, getConst))  -- base >=4.9
 
+import Control.Lens
+    ( Getting
+    , Indexed(Indexed)
+    , IndexedGetting
+    , LensLike'
+    )
 import Control.Monad.Freer (Eff, Member)
 import Control.Monad.Freer.Reader hiding (asks)
+import Data.Profunctor.Unsafe as Unsafe ((#.))
 
 
 -- | Get a specific component of the environment.
 asks :: Member (Reader e) effs => (e -> a) -> Eff effs a
 asks f = f <$> ask
+{-# INLINE asks #-}
+
+-- {{{ Lens -------------------------------------------------------------------
+
+-- | Variant of 'Control.Lens.view' that works for 'Reader' effect. See /lens/
+-- documentation for more details, and examples.
+viewEff :: Member (Reader s) effs => Getting a s a -> Eff effs a
+viewEff l = asks (getConst Unsafe.#. l Const)
+{-# INLINE viewEff #-}
+
+-- | Variant of 'Control.Lens.views' that works for 'Reader' effect. See /lens/
+-- documentation for more details, and examples.
+viewsEff
+    :: Member (Reader s) effs
+    => LensLike' (Const r) s a
+    -> (a -> r)
+    -> Eff effs r
+viewsEff l f = asks (getConst Unsafe.#. l (Const Unsafe.#. f))
+{-# INLINE viewsEff #-}
+
+-- | Variant of 'Control.Lens.iview' that works for 'Reader' effect. See /lens/
+-- documentation for more details, and examples.
+iviewEff
+    :: Member (Reader s) effs
+    => IndexedGetting i (i, a) s a
+    -> Eff effs (i, a)
+iviewEff l =
+    asks (getConst Unsafe.#. l (Indexed $ \i -> Const Unsafe.#. (,) i))
+{-# INLINE iviewEff #-}
+
+-- | Variant of 'Control.Lens.iviews' that works for 'Reader' effect. See
+-- /lens/ documentation for more details, and examples.
+iviewsEff
+    :: Member (Reader s) effs
+    => IndexedGetting i r s a
+    -> (i -> a -> r)
+    -> Eff effs r
+iviewsEff l f = asks (getConst Unsafe.#. l (Const Unsafe.#. Indexed f))
+{-# INLINE iviewsEff #-}
+
+-- }}} Lens -------------------------------------------------------------------
--- src/Control/Monad/Freer/State/Extra.hs
+++ src/Control/Monad/Freer/State/Extra.hs
@@ -12,20 +12,64 @@
 -- <https://jaspervdj.be/posts/2015-01-20-haskell-design-patterns-extended-modules.html Extended module>
 -- built on top of "Control.Monad.Freer.State".
 module Control.Monad.Freer.State.Extra
-    ( module Control.Monad.Freer.State
+    (
+    -- * Re-export Module That's Beeing Extended
+    --
+    -- | Everything is beeing re-exported from it.
+      module Control.Monad.Freer.State
+
+    -- * Effect Operations
     , gets
     , modify'
     , state
+
+    -- ** Support For Lenses
+    --
+    -- | Be aware that these definitions clash with exports from
+    -- "Control.Lens". Reason for this decision is the fact that /lens/
+    -- definitions use @MonadState@ constraint, which doesn't have such special
+    -- case as @MonadReader r ((->) r)@, therefore, these definitions aren't
+    -- applicable outside of 'State' effect context.
+
+    -- *** Lens Getters
+    , use
+    , uses
+    , iuse
+    , iuses
+
+    -- *** Lens Setters
+    , assign
+    , modifying
+    , (%=)
+    , (%%=)
+    , (?=)
     )
   where
 
 import Prelude (($!))
 
 import Control.Applicative (pure)
+import Data.Function (($), (.))
 import Data.Functor ((<$>))
+import Data.Functor.Const (Const(Const, getConst))  -- base >=4.9
+import Data.Maybe (Maybe)
 
+import Control.Lens
+    ( ASetter
+    , Getting
+    , Indexed(Indexed)
+    , IndexedGetting
+    , LensLike'
+    , Over
+    , (%~)
+    , (.~)
+    , (?~)
+    , view
+    , views
+    )
 import Control.Monad.Freer (Eff, Member)
 import Control.Monad.Freer.State
+import Data.Profunctor.Unsafe as Unsafe ((#.))
 
 
 -- | Get a specific component of the state.
@@ -47,3 +91,78 @@ state f = do
     put s
     pure a
 {-# INLINE state #-}
+
+-- {{{ Lens -------------------------------------------------------------------
+
+-- | Variant of Control.Lens.use' from "Control.Lens" that works for 'State'
+-- effect. See /lens/ documentation for more details, and examples.
+use :: Member (State s) effs => Getting a s a -> Eff effs a
+use = gets . view
+{-# INLINE use #-}
+
+-- | Variant of 'uses' that works for 'State' effect.
+uses
+    :: Member (State s) effs
+    => LensLike' (Const r) s a
+    -> (a -> r)
+    -> Eff effs r
+uses l = gets . views l
+{-# INLINE uses #-}
+
+-- | Variant of 'Control.Lens.iuse' from "Control.Lens" that works for 'State'
+-- effect. See /lens/ documentation for more details, and examples.
+iuse
+    :: Member (State s) effs
+    => IndexedGetting i (i, a) s a
+    -> Eff effs (i, a)
+iuse l = gets (getConst Unsafe.#. l (Indexed $ \i -> Const Unsafe.#. (,) i))
+{-# INLINE iuse #-}
+
+-- | Variant of 'Control.Lens.iuses' from "Control.Lens" that works for 'State'
+-- effect. See /lens/ documentation for more details, and examples.
+iuses
+    :: Member (State s) effs
+    => IndexedGetting i r s a
+    -> (i -> a -> r)
+    -> Eff effs r
+iuses l f = gets (getConst Unsafe.#. l (Const Unsafe.#. Indexed f))
+{-# INLINE iuses #-}
+
+-- | Variant of 'Control.Lens.assign' from "Control.Lens" that works for
+-- 'State' effect. See /lens/ documentation for more details, and examples.
+assign :: Member (State s) effs => ASetter s s a b -> b -> Eff effs ()
+assign l b = modify (l .~ b)
+{-# INLINE assign #-}
+
+-- | Variant of 'Control.Lens.%=' from "Control.Lens" that works for 'State'
+-- effect. See /lens/ documentation for more details, and examples.
+(%=) :: Member (State s) effs => ASetter s s a b -> (a -> b) -> Eff effs ()
+l %= f = modify (l %~ f)
+{-# INLINE (%=) #-}
+
+-- | This is an alias for ('%='), see its documentation for more details.
+modifying
+    :: Member (State s) effs
+    => ASetter s s a b
+    -> (a -> b)
+    -> Eff effs ()
+modifying = (%=)
+{-# INLINE modifying #-}
+
+-- | Variant of 'Control.Lens.%%=' from "Control.Lens" that works for 'State'
+-- effect. See /lens/ documentation for more details, and examples.
+(%%=)
+    :: Member (State s) effs
+    => Over p ((,) r) s s a b
+    -> p a (r, b)
+    -> Eff effs r
+l %%= f = state (l f)
+{-# INLINE (%%=) #-}
+
+-- | Variant of 'Control.Lens.?=' from "Control.Lens" that works for 'State'
+-- effect. See /lens/ documentation for more details, and examples.
+(?=) :: Member (State s) effs => ASetter s s a (Maybe b) -> b -> Eff effs ()
+l ?= b = modify (l ?~ b)
+{-# INLINE (?=) #-}
+
+-- }}} Lens -------------------------------------------------------------------
